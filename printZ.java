73420433829111da65171bcc4e916232f35bc9ae
wickywinner <winner312.com@gmail.com>
2021-12-30 11:45:43 +0530

    added printZ

---
+++ printZ.java
@@ -0,0 +1,27 @@
+/*Print Z
+
+ 1. You are required to print a 'z' of size 5 using '*'.                               
+Input
+
+There is no input
+
+Output
+
+print-z-in-java*/
+
+//code :-
+
+import java.util.*;
+
+public class printZ {
+
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+
+    }
+}
